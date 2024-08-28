8b33333b2d35545fbda3d1c6b110bd0278c9d737
Rohit Dhali <rohitdhali002@gmail.com>
2024-08-28 14:35:49 +0530

    Add files via upload

---
+++ python-cipher-project.py
@@ -0,0 +1,38 @@
+def vigenere(message, key, direction=1):
+    key_index = 0
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    final_message = ''
+
+    for char in message.lower():
+        # Append any non-letter character to the message
+        if not char.isalpha():
+            final_message += char
+        else:
+            # Find the right key character to encode/decode
+            key_char = key[key_index % len(key)].lower()  # Convert to lowercase
+            key_index += 1
+
+            # Define the offset and the encrypted/decrypted letter
+            offset = alphabet.index(key_char)
+            index = alphabet.find(char)
+            new_index = (index + offset * direction) % len(alphabet)
+            final_message += alphabet[new_index]
+
+    return final_message
+
+def encrypt(message, key):
+    return vigenere(message, key)
+
+def decrypt(message, key):
+    return vigenere(message, key, -1)
+
+# Get user input for the text and key
+text_to_decrypt = input("Enter the text to decrypt: ")
+custom_key = input("Enter the decryption key: ")
+
+# Decrypt the text
+decryption = decrypt(text_to_decrypt, custom_key)
+print(f'\nDecrypted text: {decryption}\n')
+
+
+
